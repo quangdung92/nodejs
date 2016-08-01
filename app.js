3f8d5fda2b31553179224bb91d24b5c97cdeffe3
dungGrooovy <mayonesama12@gmail.com>
2016-08-01 15:29:31 +0700

    complete authenticate

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var route = require('./route');
 // model
 var Model = require('./model');
 
+var app = express();
+
 passport.use(new LocalStrategy(function(username, password, done) {
     new Model.User({username: username}).fetch().then(function(data) {
         var user = data;
@@ -30,3 +32,62 @@ passport.use(new LocalStrategy(function(username, password, done) {
         }
     });
 }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+    new Model.User({username: username}).fetch().then(function(user) {
+        done(null, user);
+    });
+});
+
+
+app.set('port', process.env.PORT || 3002);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// app.use(bodyParser.json());
+
+app.use(session({secret: 'SURIMA', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// GET
+app.get('/', route.index);
+
+// signin
+// GET
+app.get('/signin', route.signIn);
+// POST
+app.post('/signin', route.signInPost);
+
+// signup
+// GET
+app.get('/signup', route.signUp);
+// POST
+app.post('/signup', route.signUpPost);
+
+// logout
+// GET
+app.get('/signout', route.signOut);
+
+/********************************/
+
+/********************************/
+// 404 not found
+app.use(route.notFound404);
+
+
+var server = app.listen(app.get('port'), function(err) {
+    if(err) throw err;
+
+    var message = 'Server is running @ http://localhost:' + server.address().port;
+    console.log(message);
+});
